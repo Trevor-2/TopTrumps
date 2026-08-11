4d08d207ff6f3b9cf48cbeaa007490ac6ff44a61
Trevor-2 <trevorlai0202@gmail.com>
2026-08-11 15:03:23 +0100

    first time

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
 import random
-nameList = ["nigga","bitch","fuck"]
-outputList = [nameList,[random.randrange(1,5)],[random.randrange(1,100)],[random.randrange(1,10)],[random.randrange(1,10)]]
-print(outputList)
+def nameList(): #easily read name from file
+    nameList = []
+    f = open("dogs.txt")
+    rawList = f.readlines()
+    f.close()
+    for name in rawList:
+        nameList.append(name.strip())
+    return nameList
+
+deck = [] # 用嚟裝所有狗仔卡嘅 List
+
+for name in nameList():
+    # 幫每隻狗仔建立一張獨立嘅卡片 (Dictionary)
+    card = {
+        "name": name,
+        "exercise": random.randrange(1, 6),      # 1 to 5
+        "intelligence": random.randrange(1, 101),  # 1 to 100
+        "friendliness": random.randrange(1, 11),   # 1 to 10
+        "drool": random.randrange(1, 11)           # 1 to 10
+    }
+    deck.append(card)
+
+print(deck)
